e54548faa7b77debc4dd0975550a556c77ad7716
Eder Andrew <ederandrew0028@gmail.com>
2020-01-19 12:55:26 -0300

    Configurando AsyncStorage

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import NetInfo from '@react-native-community/netinfo';
 import styled from 'styled-components/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 import ListContainer from './src/components/ListContainer'
 
@@ -50,41 +52,93 @@ const TitleButton = styled.Text`
   color: #fff;
 `;
 
+const VerifyNet = styled.View`
+  align-items:flex-start;
+  width:100%;
+  padding-left:20px;
+`;
+const Text = styled.Text`
+  color: #ddd;
+  font-size:12px;
+`;
+
 const Todo = () => {
   const [dados, setDados] = useState([]);
   const [input, setInput] = useState('');
+  const [netStatus, setNetStatus] = useState(0);
+
   let texto = '';
 
-  let url = 'https://b7web.com.br/todo/73938';
+  const url = 'https://b7web.com.br/todo/73938';
 
-  //Listar items
+  //Listar items direto da internet
   useEffect(() => {
-    fetch(url)
-      .then(r => r.json())
-      .then(json => {
-        let t = json.todo;
-        setDados(t)
-    })
-  },[dados])
+    //Agora verifica se esta conectado. Se estiver conectado, fluxo normal
+    switch(netStatus){
+      case 1:
+        fetch(url)
+          .then(r => r.json())
+          .then(json => {
+          let t = json.todo;
+          setDados(t)
+          //Armazena no AsyncStorage
+          let lista = JSON.stringify(json.todo)//pegando o json em forma de string
+          AsyncStorage.setItem(dados, lista)//Armazenando a string denttro do state
+        })
+        break;
+      //Se não estiver conectado, puxa do AsyncStorage
+      case 0:
+        AsyncStorage.getItem(dados).then(v => {
+          if(v != ''){
+            let listaJSON = JSON.parse(v)
+            dados = listaJSON
+          }
+          
+          setDados(dados)
+          
+        })
+        break;
+    }
+    
+  },[])
   
-  
+  //Utilizando o useEffect para que, assim que abrir o aplicativo, 
+  //o NetInfo ja identifique se o aplicativo esta online ou offline
+  useEffect(() => {
+    NetInfo.addEventListener(state => {
+      if (!state.isConnected) {
+        setNetStatus(0)
+      } else{
+        setNetStatus(1)
+      }
+    });
+  }, [])
  
   //Adicionar Item
   const addItem = () => {
-    fetch(url, {
-      method:'POST',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify({
-        item:texto
-      })
-    })
-      .then(r => r.json())
-      .then(jsont => {
-        alert('Item inserido com sucesso')
-      })
+    switch(netStatus){
+      case 1:
+        fetch(url, {
+          method:'POST',
+          headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify({
+            item:texto
+          })
+        })
+          .then(r => r.json())
+          .then(jsont => {
+            alert('Item inserido com sucesso')
+          })
+        break;
+      case 0:
+        alert('Não foi possível adicionar. Não temos conexão!');
+        break;
+
+    }
+    
   }
 
   const addButton = () => {
@@ -93,6 +147,7 @@ const Todo = () => {
     setInput(setup);
     addItem()
   }
+
   return(
     <Container>
       <Header>
@@ -109,10 +164,14 @@ const Todo = () => {
         <ButtonInput underlayColor="#ABFFD1" onPress={addButton}>
           <TitleButton>Inserir</TitleButton>
         </ButtonInput>
+        <VerifyNet>
+          <Text>{netStatus?'on-line':'off-line'}</Text>
+        </VerifyNet>
       </Footer>
     </Container>
   );
 }
 
+console.disableYellowBox = true;
 export default Todo;
 
